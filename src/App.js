33a107906f84cb50aa1d1a429d88bb779b5a9e11
HawiCaesar <brian.odhiambo@andela.com>
2020-05-06 14:20:23 +0300

    use rapidAPI, make data get fetched

--- src/App.js
+++ src/App.js
@@ -3,35 +3,43 @@ import "./styles.css";
 import { usePosition } from "./usePosition";
 
 const getURL = ({ lat, lon }) => {
-  return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_APP_APIKEY}`;
+  return `https://weatherbit-v1-mashape.p.rapidapi.com/current?lang=en&lon=${lon}&lat=${lat}`;
 };
 
 export default function App() {
   const { latitude, longitude, error } = usePosition();
-  const [weatherData, setWeatherData] = useState({});
   const [loading, setLoading] = useState(true);
-
+  const [weatherData, setWeatherData] = useState(null);
   const getWeather = async (lat, lon) => {
-    const response = await fetch(getURL({ lat, lon }));
+    const response = await fetch(getURL({ lat, lon }), {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
+        "x-rapidapi-key": process.env.REACT_APP_WEATHER_APP_APIKEY,
+      },
+    });
     return response.ok ? response.json() : Promise.reject({ error: 500 });
   };
 
-  const abc = async (lat, lon) => {
-    const response = await getWeather(lat, lon);
-    setLoading(false);
-    //setWeatherData(response);
-  };
-
   if (error) {
-    console.log(error, "#####");
-  } else if (latitude && longitude) {
-    console.log(abc(latitude, longitude));
+    return "Error in getting coordinates";
   }
 
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-      {loading ? "Loading..." : "Have weather data"}
-    </div>
-  );
+  if (latitude && longitude && weatherData === null) {
+    getWeather(latitude, longitude).then((data) => {
+      console.log("HERE WE ARE", data);
+      setLoading(false);
+      setWeatherData(data);
+    });
+  }
+
+  if (weatherData && !loading) {
+    return (
+      <div className="App">
+        <h1>Weather App</h1>
+      </div>
+    );
+  }
+
+  return "Loading...";
 }
