582bd8d3361ad126dce50fd70fb5d11e2f891340
HawiCaesar <brian.odhiambo@andela.com>
2020-05-07 13:20:25 +0300

    install and use tailwind css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./styles.css";
+import "./tailwind.generated.css";
 import { usePosition } from "./usePosition";
 
 const getURL = ({ lat, lon }) => {
@@ -8,7 +9,6 @@ const getURL = ({ lat, lon }) => {
 
 export default function App() {
   const { latitude, longitude, error } = usePosition();
-  const [loading, setLoading] = useState(true);
   const [weatherData, setWeatherData] = useState(null);
   const getWeather = async (lat, lon) => {
     const response = await fetch(getURL({ lat, lon }), {
@@ -28,15 +28,36 @@ export default function App() {
   if (latitude && longitude && weatherData === null) {
     getWeather(latitude, longitude).then((data) => {
       console.log("HERE WE ARE", data);
-      setLoading(false);
       setWeatherData(data);
     });
   }
 
-  if (weatherData && !loading) {
+  if (weatherData) {
+    let currentWeather = weatherData.data[0].weather;
     return (
       <div className="App">
-        <h1>Weather App</h1>
+        <h1 className="text-5xl text-black-700 leading-tight">Weather App</h1>
+        <h2 className="text-2xl text-black-700 leading-tight">
+          {currentWeather.description}
+        </h2>
+        <img
+          src={`https://www.weatherbit.io/static/img/icons/${currentWeather.icon}.png`}
+          height="60"
+          width="60"
+          alt={currentWeather.description}
+          className="leading-tight"
+        />
+
+        <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
+          <div className="ml-6 pt-1">
+            <h1 className="text-2xl text-blue-700 leading-tight">
+              Tailwind and Create React App
+            </h1>
+            <p className="text-base text-gray-700 leading-normal">
+              Building apps together
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
